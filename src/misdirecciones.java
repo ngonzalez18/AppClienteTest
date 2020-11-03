af770cc21607b05c32e0cf69f8a4f7a5134bbae6
Noemi Gonzalez <ngonzalez@hypernovalabs.com>
2020-11-02 19:16:27 -0500

    new changes

--- src/misdirecciones.java
+++ src/misdirecciones.java
@@ -1,27 +1,63 @@
+/*import io.appium.java_client.MobileElement;
 import io.appium.java_client.android.AndroidDriver;
 import io.appium.java_client.android.AndroidElement;
-import io.appium.java_client.remote.MobileCapabilityType;
-import org.openqa.selenium.remote.DesiredCapabilities;
 
 import java.net.MalformedURLException;
-import java.net.URL;
 
 public class misdirecciones {
 
+    public static AndroidDriver<AndroidElement> ad;
 
-    public static void main(String args[]) throws MalformedURLException {
-
-
-        DesiredCapabilities dc = new DesiredCapabilities();
-        dc.setCapability(MobileCapabilityType.DEVICE_NAME, "emulator-5554");
-        dc.setCapability("platformName", "android");
-        dc.setCapability("appPackage", "com.HNL.noveyloyalty");
-        dc.setCapability("appActivity", ".MainActivity");
-        AndroidDriver<AndroidElement> ad = new AndroidDriver<AndroidElement>(new URL("http://127.0.0.1:4723/wd/hub"), dc);
+    public static void Init(String args[]) throws MalformedURLException {
 
 
-    
+
+
+
+
+        //automation NewObj = new automation();
+        automation.ConfigInit();
+        automation.Login();
+        misdirecciones.MyDirection();
+
 
     }
 
+    public static void MyDirection () throws MalformedURLException {
+
+        MobileElement menuMiCuenta = (MobileElement) ad.findElementByXPath("/hierarchy/android.widget.FrameLayout/android.widget.LinearLayout/android.widget.FrameLayout/android.widget.LinearLayout/android.widget.FrameLayout/android.widget.FrameLayout/android.view.ViewGroup/android.view.ViewGroup[2]/android.view.ViewGroup/android.view.ViewGroup[2]/android.view.ViewGroup/android.view.ViewGroup/android.view.ViewGroup/android.view.ViewGroup/android.view.ViewGroup/android.view.ViewGroup[2]/android.view.ViewGroup[4]/android.view.ViewGroup");
+        menuMiCuenta.click();
+
+        MobileElement permisoCamaratrue = (MobileElement) ad.findElementById("com.android.permissioncontroller:id/permission_allow_button");
+        permisoCamaratrue.click();
+
+        MobileElement menuMisdirecciones = (MobileElement) ad.findElementByXPath("/hierarchy/android.widget.FrameLayout/android.widget.LinearLayout/android.widget.FrameLayout/android.widget.LinearLayout/android.widget.FrameLayout/android.widget.FrameLayout/android.view.ViewGroup/android.view.ViewGroup[2]/android.view.ViewGroup/android.view.ViewGroup[2]/android.view.ViewGroup/android.view.ViewGroup/android.view.ViewGroup/android.view.ViewGroup/android.view.ViewGroup/android.view.ViewGroup[1]/android.view.ViewGroup/android.view.ViewGroup/android.view.ViewGroup/android.view.ViewGroup/android.view.ViewGroup/android.view.ViewGroup/android.view.ViewGroup[2]/android.widget.ScrollView/android.view.ViewGroup/android.view.ViewGroup[2]/android.view.ViewGroup[5]/android.view.ViewGroup");
+        menuMisdirecciones.click();
+
+        MobileElement agregarNuevaDireccion = (MobileElement) ad.findElementByXPath("/hierarchy/android.widget.FrameLayout/android.widget.LinearLayout/android.widget.FrameLayout/android.widget.LinearLayout/android.widget.FrameLayout/android.widget.FrameLayout/android.view.ViewGroup/android.view.ViewGroup[2]/android.view.ViewGroup/android.view.ViewGroup[2]/android.view.ViewGroup/android.view.ViewGroup/android.view.ViewGroup/android.view.ViewGroup/android.view.ViewGroup/android.view.ViewGroup/android.view.ViewGroup/android.view.ViewGroup/android.view.ViewGroup/android.view.ViewGroup[2]/android.widget.ScrollView/android.view.ViewGroup/android.view.ViewGroup[1]");
+        agregarNuevaDireccion.click();
+
+        MobileElement gpsPermiso = (MobileElement) ad.findElementById("com.android.permissioncontroller:id/permission_allow_foreground_only_button");
+        gpsPermiso.click();
+
+        MobileElement escribirUbicacion = (MobileElement) ad.findElementByXPath("/hierarchy/android.widget.FrameLayout/android.widget.LinearLayout/android.widget.FrameLayout/android.widget.LinearLayout/android.widget.FrameLayout/android.widget.FrameLayout/android.view.ViewGroup/android.view.ViewGroup[2]/android.view.ViewGroup/android.view.ViewGroup[2]/android.view.ViewGroup/android.view.ViewGroup/android.view.ViewGroup/android.view.ViewGroup/android.view.ViewGroup/android.view.ViewGroup/android.view.ViewGroup/android.view.ViewGroup/android.view.ViewGroup/android.view.ViewGroup/android.view.ViewGroup[2]/android.widget.ScrollView/android.view.ViewGroup/android.view.ViewGroup[1]/android.widget.EditText");
+        escribirUbicacion.click();
+        escribirUbicacion.sendKeys("calle 50");
+
+        MobileElement scrollNuevaDireccion = (MobileElement) ad.findElementByXPath("/hierarchy/android.widget.FrameLayout/android.widget.LinearLayout/android.widget.FrameLayout/android.widget.LinearLayout/android.widget.FrameLayout/android.widget.FrameLayout/android.view.ViewGroup/android.view.ViewGroup[2]/android.view.ViewGroup/android.view.ViewGroup[2]/android.view.ViewGroup/android.view.ViewGroup/android.view.ViewGroup/android.view.ViewGroup/android.view.ViewGroup/android.view.ViewGroup/android.view.ViewGroup/android.view.ViewGroup/android.view.ViewGroup/android.view.ViewGroup/android.view.ViewGroup[2]/android.widget.ScrollView/android.view.ViewGroup");
+
+
+   //     MobileElement firstSelectedOption = selectObject.getFirstSelectedOption();
+
+
+
+
+
+    }
+
+
+
+
 }
+
+*/
